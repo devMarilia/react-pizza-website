1c08faaebb6e37e6f4622d7f51a05afccbe80e31
devMarilia <devMarilia.frontend@gmail.com>
2020-11-06 19:37:26 -0300

    GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from '../src/components/Navbar'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { GlobalStyle } from './globlaStyles';
 
 function App() {
   return (
+    
     <Router>
+      <GlobalStyle/>
       <Navbar/>
     </Router>
   );
